1696863a096f953c5f331447eb7377ecc75d564a
Ten2005 <stuzeukni0901@icloud.com>
2025-05-15 01:59:58 +0900

    mvp

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,141 @@
-import Image from "next/image";
+import { Square2StackIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const now_date = new Date();
+  const after_four_days = new Date(
+    now_date.getTime() + 4 * 24 * 60 * 60 * 1000,
+  );
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const formatDateJP = (date: Date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    return `${year}年${month}月${day}日${hours}時${minutes}分`;
+  };
+
+  const formatDateJP2 = (date: Date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    return `${year}年${month}月${day}日`;
+  };
+
+  const price = 5000;
+
+  const now_str = formatDateJP(now_date);
+  const after_four_days_str = formatDateJP(after_four_days);
+  const now_str2 = formatDateJP2(now_date);
+  const after_four_days_str2 = formatDateJP2(after_four_days);
+
+  return (
+    <div className="relative h-screen w-screen overflow-hidden">
+      <div
+        style={{ backgroundColor: "#EF7B7A" }}
+        className="absolute inset-0 z-0"
+      />
+      <div 
+        className="absolute inset-0 z-[1] opacity-20"
+        style={{
+          backgroundImage: `
+            radial-gradient(circle, rgba(255,255,255,0.8) 1px, transparent 1px),
+            radial-gradient(circle, rgba(255,255,255,0.8) 1px, transparent 1px)
+          `,
+          backgroundSize: `10px 10px, 10px 10px`,
+          backgroundPosition: `0 0, 5px 5px`,
+        }}
+      />
+      <div className="relative z-10 h-full w-full overflow-auto flex flex-col items-center gap-2 p-5">
+        <div className="absolute top-5 left-2">
+          <ChevronLeftIcon className="h-6 w-6 text-white" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex flex-col gap-2 mt-6">
+          <div className="w-16 h-16 rounded-full bg-white flex items-center justify-center mb-1 mx-auto overflow-hidden">
+            <img src="/icon.jpeg" alt="Profile" className="w-full h-full object-cover rounded-full" />
+          </div>
+          <div className="text-sm font-bold text-center">受け取りリンク</div>
+          <div className="text-sm font-bold text-center">{now_str}</div>
+        </div>
+        <div className="text-5xl flex items-center justify-center pt-6 pb-36">
+          {price}
+          <span className="text-3xl px-2 font-bold pt-1">円</span>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="text-xs font-bold border bg-amber-400 text-center w-fit mx-auto border-white rounded-full py-1 px-3">
+            受け取り待ち
+          </div>
+          <div className="text-xs font-bold ">
+            受け取り期限 {after_four_days_str}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-3 w-full mb-4 mt-2">
+          <div
+            className="text-xs font-bold border border-white rounded-full py-2 my-2 px-3 w-full flex items-center justify-between"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
+          >
+            https://pay.paypay.ne.jp/cRHaijoaw4wt
+            <span className="flex items-center gap-1">
+              <Square2StackIcon className="h-4 w-4 transform scale-x-[-1]" />
+              コピー
+            </span>
+          </div>
+          <div className="w-full text-center font-bold text-white text-sm border-2 border-white rounded-lg py-2 px-4 bg-blue-400">
+            受け取る
+          </div>
+          <div
+            className="w-full text-center font-bold text-white text-sm border-2 border-white rounded-lg py-2 px-4"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
+          >
+            辞退する
+          </div>
+        </div>
+
+        <div className="border-t border-white w-full pt-4">
+          <div
+            className="w-full rounded-lg p-4 flex flex-col gap-2"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          >
+            <div className="text-sm text-white font-bold">詳細</div>
+
+            <div className="text-xs text-white flex items-center justify-between">
+              PayPayマネー
+              <span className="font-bold">
+                {price}
+                <span className="ml-1">円</span>
+              </span>
+            </div>
+            <div className="text-xs text-white flex items-center justify-between">
+              PayPayマネーライト
+              <span className="font-bold">
+                0<span className="ml-1">円</span>
+              </span>
+            </div>
+
+            <div className="border-t border-white w-full pt-4 flex flex-col gap-2">
+              <div className="text-xs text-white flex items-center justify-between">
+                送信日<span className="font-bold">{now_str2}</span>
+              </div>
+              <div className="text-xs text-white flex items-center justify-between">
+                有効期限<span className="font-bold">{after_four_days_str2}</span>
+              </div>
+              <div className="text-xs text-white flex items-center justify-between">
+                取引番号
+                <span className="flex items-center gap-1 font-bold">
+                  09328632437605947203458
+                  <Square2StackIcon className="h-5 w-5 transform scale-x-[-1]" />
+                </span>
+              </div>
+              <div className="text-xs text-white text-center my-1">
+                PayPay 株式会社 資金移動業者 関東財務局長 第 00359 号
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
